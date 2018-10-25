e3c4b35ad28ae90ee71a44d27d87924dcecf044a
Matt Gasper <mrgasper27@gmail.com>
2018-10-24 19:01:45 -0500

    Added routes to server.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+module.exports = function (app, friends) {
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    })
+
+    app.post("api/friends", function(req, res){
+        
+    });
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Setup Express
+let app = express();
+let PORT = process.env.port || 3000;
+
+// Express data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var friends = require("./app/data/friends.js")
+
+require("./app/routing/htmlRoutes.js")(app, path);
+require("./app/routing/apiRoutes.js")(app, friends);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
